743051a97641a59d8ea4b6a1368756255224b6a2
Jackson Mayfield <jacksonm.tn@gmail.com>
2024-09-05 20:33:05 -0500

    Fixed moisture level reading

--- arduino_code/src/main.cpp
+++ arduino_code/src/main.cpp
@@ -3,8 +3,8 @@
 /*
 Capacitive Soil Moisture Sensor Read In
 */
-const int dry = 463; //Constant for dry sensor
-const int wet = 195; //Constant for wet sensor
+const int dry = 463; // Constant for dry sensor
+const int wet = 195; // Constant for wet sensor
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -16,4 +16,8 @@ void loop() {
   int sensor_val = analogRead(A0);
   // Translates the analog read to a percentage between 0 - 100% moisture content
   int per_moisture = map(sensor_val, wet, dry, 100, 0);
+  // Convert the integer to a String and concatenate with other text
+  String moisture_str = String(per_moisture);
+  Serial.println("Moisture level: " + moisture_str + "%");
+  delay(1000);
 }
